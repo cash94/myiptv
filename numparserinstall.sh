7037f1a92b9746d2ccede9248b0bfced529949d5
cash94 <artemhasan@gmail.com>
2025-05-20 17:27:29 +0300

    Update numparserinstall.sh

--- numparserinstall.sh
+++ numparserinstall.sh
@@ -1,21 +1,31 @@
+#!/bin/bash
+
+# Запрашиваем у пользователя TMDb токен
+read -p "Введите ваш TMDb Bearer токен: " tmdb_token
+
+# Создаём директорию
 sudo mkdir /home/nmprs
 
+# Назначаем владельца
 sudo chown -R $USER:$USER /home/nmprs
 
-sudo wget https://github.com/trinity-aml/NUMParser/releases/download/numParser_1.7/numParser-linux-amd64 -O /home/nmprs/numParser-linux-amd64
+# Скачиваем бинарник
+sudo wget https://github.com/trinity-aml/NUMParser/releases/download/numParser_1.7/numParser-linux-amd64  -O /home/nmprs/numParser-linux-amd64
 
+# Делаем его исполняемым
 sudo chmod o+x /home/nmprs/numParser-linux-amd64
 
+# Создаём systemd-юнит
 echo -e '[Unit]\nDescription=nmprs\nWants=network.target\nAfter=network.target\n\n[Service]\nWorkingDirectory=/home/nmprs\nExecStart=/home/nmprs/numParser-linux-amd64\nRestart=always\n\n[Install]\nWantedBy=multi-user.target' | sudo tee /etc/systemd/system/nmprs.service > /dev/null
 
-echo -e 'port: 9090\nhost: http://rutor.info\ntmdbtoken: Bearer syda svoi key' | sudo tee /home/nmprs/config.yml > /dev/null
+# Создаём конфиг с введённым токеном
+echo -e "port: 9090\nhost: http://rutor.info\ntmdbtoken: Bearer $tmdb_token" | sudo tee /home/nmprs/config.yml > /dev/null
 
+# Перезагружаем демон systemd
 sudo systemctl daemon-reload
 
+# Запускаем и включаем сервис
 sudo systemctl start nmprs
-
 sudo systemctl enable nmprs
 
-sudo apt install nano
-
-sudo nano /home/nmprs/config.yml
+echo "Установка завершена. Сервис nmprs запущен на порту 9090."
